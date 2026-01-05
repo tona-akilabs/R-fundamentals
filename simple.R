4a12621c096988a62cf91ed1a9dbd705a8f1ff36
Tona <tona.chheun@akilabs.co>
2026-01-05 13:36:00 +0700

    add

--- simple.R
+++ simple.R
@@ -1,3 +1,14 @@
-print("Hello World!")
+#print("Hello World!")
+#print(4 + 5)
 
-print(4 + 5)
+
+fruits <- c("Apply", "Banana", "Cherry")
+print(fruits)
+
+years <- c(2025, 2026, 2027)
+names(years) <- c("Previously", "Current", "Next")
+print(years)
+print(years["Next"])
+
+df <- data.frame(index = 1:5, dat = rnorm(5))
+print(df)
